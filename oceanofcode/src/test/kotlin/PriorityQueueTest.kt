2682ce5e2c5aa2bfa919230cb0fc72f13847cdbb
Amaury Ollagnier <amaury.ollagnier@gmail.com>
2020-03-23 22:43:28 +0100

    Test Priority Queue

---
+++ oceanofcode/src/test/kotlin/PriorityQueueTest.kt
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.Comparator
+
+class PriorityQueueTest {
+
+  @Test
+  fun test() {
+    val to = Vector2D(4, 4);
+    val queue = PriorityQueue<Vector2D>(Comparator { t1, t2 -> (t1.distance(to) - t2.distance(to)).toInt() })
+
+    queue.add(Vector2D(1, 1))
+    queue.add(Vector2D(3, 3))
+    queue.add(Vector2D(2, 2))
+    queue.add(Vector2D())
+
+    Assertions.assertEquals(Vector2D(3, 3), queue.poll())
+  }
+}
